98dafd30eeafe7890a6c94810dcda01364716f9e
Peter Gribanov <wcode404@gmail.com>
2017-03-22 14:00:29 +0300

    create Doctrine DBAL types for IPv6Interval and IPv6Network

---
+++ src/Persistence/Doctrine/DBAL/Types/IPv6IntervalType.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * GpsLab component.
+ *
+ * @author    Peter Gribanov <info@peter-gribanov.ru>
+ * @copyright Copyright (c) 2016, Peter Gribanov
+ * @license   http://opensource.org/licenses/MIT
+ */
+
+namespace GpsLab\Component\Interval\Persistence\Doctrine\DBAL\Types;
+
+use GpsLab\Component\Interval\IPv6\IPv6Interval;
+
+class IPv6IntervalType extends BaseType
+{
+    /**
+     * @return string
+     */
+    protected function getIntervalClass()
+    {
+        return IPv6Interval::class;
+    }
+}
